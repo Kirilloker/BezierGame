bc9718ddf14e5f09470dc8a3b8cceacbf8904669
Егор <89208912+Kirilloker@users.noreply.github.com>
2022-09-25 04:38:22 +0300

    test commit

--- Assets/Code/Cheat.cs
+++ Assets/Code/Cheat.cs
@@ -13,32 +13,12 @@ public class Cheat : MonoBehaviour
     public void GiveMoney()
     {
         data.Coins += 100;
-<<<<<<< HEAD
-        UpgradeEffect effect = UpgradeEffect.speed;
-
-        switch (effect)
-        {
-            case UpgradeEffect.speed:
-                Debug.Log("Добавить улучшение 1");
-                goto case UpgradeEffect.size;
-            case UpgradeEffect.size:
-                Debug.Log("Добавить улучшение 2");
-                goto case UpgradeEffect.shield;
-            case UpgradeEffect.shield:
-                Debug.Log("Добавить улучшение 3");
-                break;
-            default:
-                break;
-        }
-
     }
 
     public void ResetData()
     {
         Debug.Log("Удаляются данные");
         data.ResetData();
-=======
->>>>>>> parent of c29e3f8 (Р”РѕР±Р°РІР»РµРЅР° РєРЅРѕРїРєР° РЅР°Р·Р°Рґ РІ РјР°РіР°Р·РёРЅРµ Рё РєРѕР»РёС‡РµСЃС‚РІРѕ РјРѕРЅРµС‚)
     }
 
 }
